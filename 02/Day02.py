bd12784017c989caa6417dc51f612438c895aa79
LucienPep <xxyyzzoozz@gmail.com>
2023-09-23 23:57:03 +0100

    Completed Part 2 and completed Day02

--- 02/Day02.py
+++ 02/Day02.py
@@ -1,17 +1,20 @@
 with open("PositionData.txt", "r") as rawData:
     positionData = rawData.readlines()
 
-horizontal = vertical = 0
+horizontal = vertical = depth = 0
 
 for line in positionData:
     movement = int(line.split()[1])
     x = line[0]
     if x == "f":
         horizontal += movement
+        depth += (movement * vertical) 
     elif x == "u":
         vertical -= movement
     else:
         vertical += movement
 
 finalResult = vertical * horizontal
+depthResult = depth * horizontal
 print(finalResult)
+print(depthResult)
